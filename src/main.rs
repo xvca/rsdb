a4b35b939af1f6365d8fecac8ec49ceac0a0eb9c
xvca <dev@luca.vin>
2026-01-31 14:07:31 +1100

    add basic repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("rsdb - a sqlite clone in rust");
+    loop {
+        print!("db > ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("failed to read line");
+
+        let input = input.trim();
+
+        match input {
+            ".exit" => break,
+            _ => eprintln!("unrecognized command: {}", input),
+        }
+    }
 }
